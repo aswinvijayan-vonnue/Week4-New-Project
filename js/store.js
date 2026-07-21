1492a39590225058d9856a93abc159220a2944eb
aswinvijayan-vonnue <aswin.vijayan@vonnue.com>
2026-07-21 10:56:05 +0530

    feat:changed state manager code

--- js/store.js
+++ js/store.js
@@ -1,18 +1,65 @@
-export function createStore(initialState, reducer) {
+function createStore(initialState, reducer) {
   let state = initialState;
-  let listeners = [];
-  const getState = () => {
-    return state;
+  const listeners = [];
+  return {
+    getState() {
+      return state;
+    },
+    dispatch(action) {
+      // Update state
+      state = reducer(state, action);
+      console.log('state', state);
+      // Notify subscribers
+      listeners.forEach((listener) => listener(state));
+    },
+    subscribe(listener) {
+      listeners.push(listener);
+      // TODO: Return unsubscribe function
+    },
   };
-  const dispath = (action) => {
-    state = reducer(state, action);
-    listeners.forEach((listener) => listener());
-  };
-  const subscribe = (listener) => {
-    listeners.push(listener);
-    return () => {
-      listeners = listeners.filter((singleLi) => singleLi != listener);
-    };
-  };
-  return { getState, dispath, subscribe };
 }
+
+const initialState = {
+  route: {
+    path: '/',
+    params: {},
+  },
+};
+function reducer(state, action) {
+  console.log(state);
+  switch (action.type) {
+    case 'ROUTE_CHANGED':
+      return {
+        ...state,
+        route: action.payload,
+      };
+    case 'CREATE_MOVIE':
+      const currentMovies = state.movies || [];
+      return {
+        ...state,
+        movies: [...currentMovies, action.payload],
+      };
+    case 'USER_CHANGED':
+      return {
+        ...state,
+        username: action.payload,
+      };
+
+    default:
+      return state;
+  }
+}
+export const store = createStore(initialState, reducer);
+
+function onRouteChange(path, params) {
+  store.dispatch({
+    type: 'ROUTE_CHANGED',
+    payload: {
+      path,
+      params,
+    },
+  });
+}
+// store.subscribe((state) => {
+//   console.log(state.route);
+// });
